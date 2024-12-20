a12228c8bbd9160d0a2a49f08a7140f770414308
Alin Lupascu <lupascu.marius@gmail.com>
2024-12-20 09:37:06 -0700

    Initial Commit

---
+++ PearHID/PearHIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  PearHIDApp.swift
+//  PearHID
+//
+//  Created by Alin Lupascu on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PearHIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
